d263525f5aa02f97b8e34af9217b36556ff996ad
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-26 13:56:11 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,21 @@
 def main():
     #write your code below this line
+    fileName = input("File?")
+    lowerBound = int(input("Lower bound?"))
+    upperBound = int(input("Upper bound?"))
+    noNumbers = 0
+    try:
+      with open(fileName,'r') as f:
+        lines = f.read().splitlines()
+        for number in lines:
+          if int(number) >=lowerBound and int(number)<=upperBound:
+            noNumbers += 1
+        print("Numbers: %s"%noNumbers)
+
+    except:
+        print("Reading the file %s failed."%fileName)
+
+
 
 if __name__ == '__main__':
     main()
